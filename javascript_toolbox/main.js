df466edded86c192d08f20a5e755886d4df1c87a
hongzzi-ca <hongzzi3366@gmail.com>
2020-10-21 20:57:35 -0500

    add

--- javascript_toolbox/main.js
+++ javascript_toolbox/main.js
@@ -9,12 +9,53 @@ $(document).ready(function(){
         $("body").css("background", "purple");
 
         $(this).css("fontSize", fontSize);
-        fontSize = fontSize + 5;
+        fontSize = fontSize + 10;
+
+        $("#box").addClass("my-special-rotate-class");
 
         $("div").css("color", "red");
     })
 
-    
+    let shouldAddText = true;
+
+    // Mouse wheel event: Trigger an effect when the user scrolls
+    $(document).on("wheel", function() {
+        //console.log("scrolling happened!");
+
+        let coolBox = `<div class="cool-box"></div>`;
+        let amountScrolled = $(document).scrollTop();
+
+        console.log("scroll: " + amountScrolled);
+
+        if(amountScrolled > 1000 && shouldAddText == true) {
+            $("body").css("background", "pink");
+            let p = `<p class= "some-text">"""""someText"""""</p>`;
+
+            //아주 중요한 부분 backtick 쓰는 방법 여기에서 p는 윗줄을 말함
+            $("body").append(p)
+            $(".some-text").css("top", `${amountScrolled + 400}px`);
+            shouldAddText = false;
+        }
+
+        $("body").append(coolBox);
+    })
+
+    setInterval(function) {
+        let coolBox = `<div class="cool-box"></div>`;
+        $("body").append(coolBox);
+
+        //랜덤으로 색 변하게
+        let randomR = Math.floor(Math.random() * 256);
+        let randomG = Math.floor(Math.random() * 256);
+        let randomB = Math.floor(Math.random() * 256);
+
+        $(".cool-bos").css("background", "rgb"(${randomR}, ${randomG},${randomB})`)
+    }, 1000)
+
+    setTimeout(function() {
+        alert("it has been 3 seconds");
+    }, 3000)
+
 
     function printHello() {
         console.log("hello");
